9d8cb10b663bcf48a3eac7d22592bc4991c07485
franklin <frankagu92@gmail.com>
2025-04-06 12:41:34 +0100

    updated readme file

--- mocks/trendData.ts
+++ mocks/trendData.ts
@@ -28,18 +28,18 @@ export const mockTrendData: Record<string, HashtagTrendData> = {
       { date: '2025-04-07', sentiment: 0.3 }
     ]
   },
-  react: {
-    hashtag: '#react',
+  angular: {
+    hashtag: '#angular',
     range: 'Apr 1 - Apr 7, 2025',
     trend: [
-      { date: '2025-04-01', sentiment: 0.1 },
-      { date: '2025-04-02', sentiment: -0.1 },
-      { date: '2025-04-03', sentiment: -0.2 },
-      { date: '2025-04-04', sentiment: 0.0 },
-      { date: '2025-04-05', sentiment: 0.2 },
-      { date: '2025-04-06', sentiment: 0.3 },
-      { date: '2025-04-07', sentiment: 0.4 }
-    ]
+        { date: '2025-04-01', sentiment: 0.5 },
+        { date: '2025-04-02', sentiment: 0.4 },
+        { date: '2025-04-03', sentiment: 0.3 },
+        { date: '2025-04-04', sentiment: 0.2 },
+        { date: '2025-04-05', sentiment: 0.1 },
+        { date: '2025-04-06', sentiment: -0.1 },
+        { date: '2025-04-07', sentiment: -0.2 }
+      ]
   },
   typescript: {
     hashtag: '#typescript',
